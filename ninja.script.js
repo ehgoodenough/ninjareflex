6b6016133db804c48e578ce57b50fab10a84f3ae
Arcym <andrewmcp333@gmail.com>
2013-09-12 10:04:00 -0400

    Defined a two dimensional array for the stage.

--- ninja.script.js
+++ ninja.script.js
@@ -1,5 +1,20 @@
 var ninja = { position: {x: 0} }
 
+var stage = 
+[
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+];
+
 $(window).keyup(function(event)
 {
 	if(event.keyCode == 39)
@@ -13,9 +28,26 @@ $(window).keyup(function(event)
 	
 	$screen = $("canvas#screen");
 	$screen.clearCanvas();
+	for(y = 0; y < 11; y++)
+	{
+		for(x = 0; x < 16; x++)
+		{
+			if(stage[y][x] == 1)
+			{
+				$screen.drawRect(
+				{
+					x: x*32, y: y*32,
+					width: 32, height: 32,
+					fillStyle: "#000",
+					fromCenter:false
+				});
+			}
+		}
+	}
 	$screen.drawRect(
 	{
-		x: ninja.position.x*32, y: 360-32,
+		x: ninja.position.x*32,
+		y: 352-32-32,
 		width: 32, height: 32,
 		fillStyle: "green",
 		fromCenter:false
