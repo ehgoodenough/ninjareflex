a21ca16efdf90083395564697771ae30cc59fa34
Arcym <andrewmcp333@gmail.com>
2013-09-15 07:25:18 -0400

    Rearranged the code for handling each keystroke.

--- ninja.script.js
+++ ninja.script.js
@@ -17,7 +17,7 @@ var ninja =
 	{
 		this.position.y--;
 	},
-	handleGravity: function()
+	moveDown: function()
 	{
 		if(!this.hasDownwardsCollision())
 		{
@@ -59,25 +59,27 @@ $(window).keyup(function(event)
 	if(event.keyCode == 39)
 	{
 		ninja.moveRight();
+		ninja.moveDown();
 	}
 	else if(event.keyCode == 37)
 	{
 		ninja.moveLeft();
+		ninja.moveDown();
 	}
-	
-	if(event.keyCode == 39
-	|| event.keyCode == 37
-	|| event.keyCode == 40)
+	else if(event.keyCode == 40)
 	{
-		ninja.handleGravity();
+		ninja.moveDown();
 	}
-	
-	if(event.keyCode == 38)
+	else if(event.keyCode == 38)
 	{
 		if(ninja.jump > 0)
 		{
 			ninja.moveUp();
 			ninja.jump--;
+		}
+		else
+		{
+			ninja.moveDown();
 		}
 	}
 	
